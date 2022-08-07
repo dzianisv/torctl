a3dfe1f680c1678d51d77eca47a768b820d6a40c
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2022-08-07 14:17:12 -0700

    Add test

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 for type in deb "$@"; do
-    fpm --input-type dir --output-type "${type}" --depends curl --architecture all -C package/ -n "$(basename `git rev-parse --show-toplevel`)" --version "$(git describe --tags)"
+    fpm --input-type dir --output-type "${type}" --depends "tor" --architecture all -C package/ -n "$(basename `git rev-parse --show-toplevel`)" --version "$(git describe --tags)"
 done
