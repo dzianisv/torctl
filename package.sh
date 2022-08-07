5e71b78a1196773cd92ff84951d6ef4f416ae48f
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2022-08-07 14:26:32 -0700

    Update version definition

--- package.sh
+++ package.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -eu
+tag=$(git describe --tags)
+version=${tag%%-*}
+
 for type in deb "$@"; do
-    fpm --input-type dir --output-type "${type}" --depends "tor" --architecture all -C package/ -n "$(basename `git rev-parse --show-toplevel`)" --version "$(git describe --tags)"
+    fpm --input-type dir --output-type "${type}" --depends "tor" --architecture all -C package/ -n "$(basename `git rev-parse --show-toplevel`)" --version "${version}"
 done
